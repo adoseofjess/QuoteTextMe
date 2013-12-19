f353c652d26a69cacd4baf4863b20425e9b957b0
Jessica J. Kim <jessjkim@gmail.com>
2013-12-19 15:17:05 -0800

    before heroku

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,15 +2,15 @@ require 'sinatra'
 
 class QuotesController < ApplicationController
   def get_quote_by_category
-    twiml = Twilio::TwiML::Response.new do |r|
-        r.Message "Hey Monkey. Thanks for the message!"
-      end
+    # twiml = Twilio::TwiML::Response.new do |r|
+    #     r.Message "Hey Monkey. Thanks for the message!"
+    #   end
     puts "get quote by category reached"
-    twiml.text
+    # twiml.text
     
     
-    # category = params[:category]
-#     quote = Quote.find_all_by_category(category).sample.body
-#     render :json => quote
+    category = params[:category]
+    quote = Quote.find_all_by_category(category).sample.body
+    render :json => quote
   end
 end
