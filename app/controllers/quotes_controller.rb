e1b72bc9cbde5d4195439438e7d242503220fcd1
Jessica J. Kim <jessjkim@gmail.com>
2014-01-17 14:47:24 -0800

    Fix selecting quotes only under 160.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -54,8 +54,8 @@ class QuotesController < ApplicationController
     
       quote1 = Quote.find_all_by_category(category).sample.body
       
-      quote2 = Quote.find_all_by_category(category).find_all { |quote| quote.length < 160 }.sample.body
-      
+      quote2 = Quote.find_all_by_category(category).find_all { |quote| quote.body.length < 160 }.sample.body
+      debugger
       
       @twilio_client.account.sms.messages.create(
             :from => "+1#{twilio_phone_number}",
